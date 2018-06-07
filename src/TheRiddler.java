e692397bda4356a6c87a9ae081435c384b523905
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-07 13:23:09 -0700

    Riddler, Robot in space ip

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -7,17 +7,24 @@ import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 
 		// 1. Make a variable to hold the score
+		String answer = "Map";
 		int score =0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-		JOptionPane.showInputDialog("I have cities but no houses, I have mountains but no trees, I have water but no fish. What am I?)
+		String guess = JOptionPane.showInputDialog("I have cities but no houses, I have mountains but no trees, I have water but no fish. What am I?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (guess.equals(answer) ) {
+	JOptionPane.showMessageDialog(null, "You are Super smart!!");
+	score = score +1;
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+else {
+	JOptionPane.showMessageDialog(null, "Wrong");
+}
 		// 6. Add some more riddles
+
 
 		// 2. Make a pop up to show the score.
 		
